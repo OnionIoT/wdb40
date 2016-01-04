51663180dca3844732ffd4d67f8defb499a5c48e
greenbreakfast <lazardemin@gmail.com>
2016-01-04 18:43:46 -0500

    cleaned up how uci network config is parsed into network objects, compartmentalized into functions; added parsing of network mode

--- include/uci_intf.h
+++ include/uci_intf.h
@@ -25,10 +25,16 @@ extern "C" {
 #define UCI_INTF_WIFI_IFACE_OPT_DISABLED 	"disabled"
 #define UCI_INTF_WIFI_IFACE_OPT_KEY 		"key"
 
-#define UCI_INTF_WIFI_IFACE_ENCRYPTION_TYPE_NONE 	"none"
-#define UCI_INTF_WIFI_IFACE_ENCRYPTION_TYPE_WEP 	"wep"
-#define UCI_INTF_WIFI_IFACE_ENCRYPTION_TYPE_PSK 	"psk"
-#define UCI_INTF_WIFI_IFACE_ENCRYPTION_TYPE_PSK2 	"psk2"
+#define UCI_INTF_WIFI_IFACE_ENCRYPTION_TYPE_NONE 		"none"
+#define UCI_INTF_WIFI_IFACE_ENCRYPTION_TYPE_WEP 		"wep"
+#define UCI_INTF_WIFI_IFACE_ENCRYPTION_TYPE_PSK 		"psk"
+#define UCI_INTF_WIFI_IFACE_ENCRYPTION_TYPE_PSK2 		"psk2"
+
+#define UCI_INTF_WIFI_IFACE_DISABLED_VALUE_TRUE 		"1"
+#define UCI_INTF_WIFI_IFACE_DISABLED_VALUE_FALSE 		"0"
+
+#define UCI_INTF_WIFI_IFACE_MODE_VALUE_AP 				"ap"
+#define UCI_INTF_WIFI_IFACE_MODE_VALUE_STA		 		"sta"
 
 // class to perform all iwinfo operations
 class uciIntf : public Module {
@@ -38,21 +44,29 @@ public:
 
 	void 	Reset					();
 
-	void 	ReleaseBackend 			();
+	void 	GetNetworkListSize		(int &output);
+	int 	GetNetworkList 			(std::vector<networkInfo> &list);
 
 	int 	ReadBackend 			();
+	int 	ReleaseBackend 			();
+
 	int 	ReadWirelessConfig		();
 	int 	ProcessConfigData		();
 
-	void 	GetNetworkListSize		(int &output);
-	int 	GetNetworkList 			(std::vector<networkInfo> &list);
+	
 
 
 
 
 private:
 	// private functions
-	void 	_formatEncryption 		(const char* input, int &encryptionType);
+	int 	_ParseWirelessSection	(struct uci_section *s, const char* sectionName);
+
+	int 	_ParseEncryption 		(const char* input);
+	int 	_ParseDisabled			(const char* input);
+	int 	_ParseMode				(const char* input);
+
+
 
 	// private members
 	struct uci_context 			*ctx;
--- src/uci_intf.cpp
+++ src/uci_intf.cpp
@@ -22,16 +22,26 @@ void uciIntf::Reset()
 	bBackendInitialized 	= 0;
 }
 
-// free the uci context
-void uciIntf::ReleaseBackend()
+
+
+//// functions related to network list
+void uciIntf::GetNetworkListSize(int &output)
 {
-	if (ctx != NULL && bBackendInitialized == 1) {
-		bBackendInitialized = 0;
-		uci_free_context (ctx);
-	}
+	output 	= networkList.size();
 }
 
+int uciIntf::GetNetworkList(std::vector<networkInfo> &list)
+{
+	if (networkList.size() > 0) {
+		list 	= networkList;
 
+		return EXIT_SUCCESS;
+	}
+
+	return EXIT_FAILURE;
+}
+
+//// UCI context functions
 // Allocate the UCI context
 int uciIntf::ReadBackend()
 {
@@ -48,6 +58,21 @@ int uciIntf::ReadBackend()
 	return EXIT_SUCCESS;
 }
 
+// free the uci context
+int uciIntf::ReleaseBackend()
+{
+	if (ctx != NULL && bBackendInitialized == 1) {
+		bBackendInitialized = 0;
+		uci_free_context (ctx);
+
+		return EXIT_SUCCESS;
+	}
+
+	return EXIT_FAILURE;
+}
+
+
+//// UCI processing functions
 // Read the UCI wireless configuration
 int uciIntf::ReadWirelessConfig()
 {
@@ -63,16 +88,15 @@ int uciIntf::ReadWirelessConfig()
 	return EXIT_SUCCESS;
 }
 
+// Process the config data
 int uciIntf::ProcessConfigData()
 {
 	int 	status = EXIT_FAILURE;
 	
 	struct 	uci_element 	*e;
 	struct 	uci_section 	*s;
-	struct 	uci_option 		*o;
 
 	networkInfo				*network;
-	int 					encr;
 	
 	// ensure that config has been read
 	if (wirelessPtr.target == UCI_TYPE_PACKAGE) {
@@ -83,27 +107,8 @@ int uciIntf::ProcessConfigData()
 				// grab a pointer to the section
 				s = uci_lookup_section(ctx, wirelessPtr.p, e->name);
 
-				// only look at wifi network setup
-				if ( strncmp(s->type, UCI_INTF_WIFI_IFACE, strlen(UCI_INTF_WIFI_IFACE) ) == 0) {
-					// lookup the network info 
-					//o = (ctx, s, UCI_INTF_WIFI_IFACE_OPT_SSID);
-					/*_Print(1, "  section: %s\n", e->name);
-					_Print(1, "    ssid:     %s\n", uci_lookup_option_string(ctx, s, UCI_INTF_WIFI_IFACE_OPT_SSID) );
-					_Print(1, "    encr:     %s\n", uci_lookup_option_string(ctx, s, UCI_INTF_WIFI_IFACE_OPT_ENCRYPTION) );
-					_Print(1, "    disabled: %s\n", uci_lookup_option_string(ctx, s, UCI_INTF_WIFI_IFACE_OPT_DISABLED) );*/
-
-					// populate wifiNetwork object
-					_formatEncryption(uci_lookup_option_string(ctx, s, UCI_INTF_WIFI_IFACE_OPT_ENCRYPTION), encr);
-					network 	= new networkInfo(uci_lookup_option_string(ctx, s, UCI_INTF_WIFI_IFACE_OPT_SSID), encr);
-					network->SetConfigName(e->name);
-					network->SetVerbosity(1);
-
-					// append to vector class member
-					networkList.push_back(*network);
-
-					// free the memory
-					delete network;
-				} // if WIFI_IFACE
+				// parse the section into a network info object
+				status 	= _ParseWirelessSection(s, e->name);
 			} // if UCI_TYPE_SECTION
 
 		} // foreach uci element	
@@ -111,46 +116,127 @@ int uciIntf::ProcessConfigData()
 		status 	=  EXIT_SUCCESS;
 	} // if UCI_TYPE_PACKAGE
 
-	
 
 	return status;
 }
 
-void uciIntf::_formatEncryption(const char* input, int &encryptionType)
+
+//// Parsing functions
+// parse config wireless wifi-iface section 
+int uciIntf::_ParseWirelessSection(struct uci_section *s, const char* sectionName)
 {
-	// default is unknown
-	encryptionType 		= WDB40_ENCRYPTION_UNKNOWN;
+	int 	status;
 
-	if (strcmp(input, UCI_INTF_WIFI_IFACE_ENCRYPTION_TYPE_NONE) == 0 ) {
-		encryptionType 	= WDB40_ENCRYPTION_NONE;
-	}
-	else if (strcmp(input, UCI_INTF_WIFI_IFACE_ENCRYPTION_TYPE_WEP) == 0 ) {
-		encryptionType 	= WDB40_ENCRYPTION_WEP;
-	}
-	else if (strcmp(input, UCI_INTF_WIFI_IFACE_ENCRYPTION_TYPE_PSK) == 0 ) {
-		encryptionType 	= WDB40_ENCRYPTION_PSK;
-	}
-	else if (strcmp(input, UCI_INTF_WIFI_IFACE_ENCRYPTION_TYPE_PSK2) == 0 ) {
-		encryptionType 	= WDB40_ENCRYPTION_PSK2;
-	}
-}
+	networkInfo				*network;
+	int 					tmp;
+
+	// only look at wifi network setup
+	if ( strncmp(s->type, UCI_INTF_WIFI_IFACE, strlen(UCI_INTF_WIFI_IFACE) ) == 0) {
+		// lookup the network info 
+		/*//o = (ctx, s, UCI_INTF_WIFI_IFACE_OPT_SSID);
+		_Print(1, "  section: %s\n", e->name);
+		_Print(1, "    ssid:     %s\n", uci_lookup_option_string(ctx, s, UCI_INTF_WIFI_IFACE_OPT_SSID) );
+		_Print(1, "    encr:     %s\n", uci_lookup_option_string(ctx, s, UCI_INTF_WIFI_IFACE_OPT_ENCRYPTION) );
+		_Print(1, "    disabled: %s\n", uci_lookup_option_string(ctx, s, UCI_INTF_WIFI_IFACE_OPT_DISABLED) );*/
 
 
-void uciIntf::GetNetworkListSize(int &output)
-{
-	output 	= networkList.size();
-}
+		// experiment with lookup next function
+		/*status 	= uci_lookup_next(ctx, &ee, &s->options, UCI_INTF_WIFI_IFACE_OPT_SSID);
+		o 		= uci_to_option(ee);
+		_Print(1, "    element name: %s, type = %d\n", ee->name, ee->type); */
 
-int uciIntf::GetNetworkList(std::vector<networkInfo> &list)
-{
-	if (networkList.size() > 0) {
-		list 	= networkList;
+
+
+
+		//// populate wifiNetwork object
+		network 	= new networkInfo( uci_lookup_option_string(ctx, s, UCI_INTF_WIFI_IFACE_OPT_SSID) );
+		network->SetVerbosity(1);
+
+		// find the encryption type
+		tmp 		= _ParseEncryption(	uci_lookup_option_string(ctx, s, UCI_INTF_WIFI_IFACE_OPT_ENCRYPTION) );
+		network->SetEncryptionType(tmp);
+		
+		// note the wireless section name
+		network->SetConfigName(sectionName);
+		
+		// check if network is disabled
+		tmp 		= _ParseDisabled( uci_lookup_option_string(ctx, s, UCI_INTF_WIFI_IFACE_OPT_DISABLED) );
+		network->SetDisabled(tmp);
+
+		// find the network mode
+		tmp 		= _ParseMode( uci_lookup_option_string(ctx, s, UCI_INTF_WIFI_IFACE_OPT_MODE) );
+		network->SetNetworkMode(tmp);
+
+
+		//// append to vector class member
+		networkList.push_back(*network);
+
+		// free the memory
+		delete network;
 
 		return EXIT_SUCCESS;
-	}
+	} // if WIFI_IFACE
 
 	return EXIT_FAILURE;
 }
+
+int uciIntf::_ParseEncryption(const char* input)
+{
+	int output;
+
+	// default is unknown
+	output 		= WDB40_ENCRYPTION_UNKNOWN;
+
+	if (strcmp(input, UCI_INTF_WIFI_IFACE_ENCRYPTION_TYPE_NONE) == 0 ) {
+		output 	= WDB40_ENCRYPTION_NONE;
+	}
+	else if (strcmp(input, UCI_INTF_WIFI_IFACE_ENCRYPTION_TYPE_WEP) == 0 ) {
+		output 	= WDB40_ENCRYPTION_WEP;
+	}
+	else if (strcmp(input, UCI_INTF_WIFI_IFACE_ENCRYPTION_TYPE_PSK) == 0 ) {
+		output 	= WDB40_ENCRYPTION_PSK;
+	}
+	else if (strcmp(input, UCI_INTF_WIFI_IFACE_ENCRYPTION_TYPE_PSK2) == 0 ) {
+		output 	= WDB40_ENCRYPTION_PSK2;
+	}
+
+	return output;
+}
+
+int uciIntf::_ParseDisabled(const char* input)
+{
+	int output;
+
+	// default is disabled
+	output 		= WDB40_ENCRYPTION_UNKNOWN;
+
+	if (strcmp(input, UCI_INTF_WIFI_IFACE_DISABLED_VALUE_TRUE) == 0 ) {
+		output 	= 1;
+	}
+	else if (strcmp(input, UCI_INTF_WIFI_IFACE_DISABLED_VALUE_FALSE) == 0 ) {
+		output 	= 0;
+	}
+
+	return output;
+}
+
+int uciIntf::_ParseMode(const char* input)
+{
+	int output;
+
+	// default is unkown
+	output 		= WDB40_NETWORK_MODE_UNKNOWN;
+
+	if (strcmp(input, UCI_INTF_WIFI_IFACE_MODE_VALUE_AP) == 0 ) {
+		output 	= WDB40_NETWORK_AP;
+	}
+	else if (strcmp(input, UCI_INTF_WIFI_IFACE_MODE_VALUE_STA) == 0 ) {
+		output 	= WDB40_NETWORK_STA;
+	}
+
+	return output;
+}
+
 
 
 
